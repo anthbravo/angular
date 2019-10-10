e72595bb0175ef34133d83767ac66ff408f3eb79
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2019-10-09 22:55:15 -0500

    Se desarrolla los componentes productos y se estudia el ciclo de vida de un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzi-store';
+
+  products: Array<Product> = [
+    {
+      id: '1',
+      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      title: 'Camisa Casual Azul',
+      price: 1000,
+      description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
+    },
+    {
+      id: '2',
+      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      title: 'Camisa Casual Azul',
+      price: 1000,
+      description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
+    },
+    {
+      id: '3',
+      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      title: 'Camisa Casual Azul',
+      price: 1000,
+      description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
+    }
+
+  ];
+
+  addCart(id: string) {
+
+    console.log(id);
+  }
+
 }
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,49 @@
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChange,
+    OnInit,
+    DoCheck,
+    OnDestroy
+} from '@angular/core';
+
+import { Product } from '../product.model';
+
+@Component(
+    {
+        selector: 'app-product',
+        templateUrl: './product.component.html'
+    }
+)
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
+    @Input() product: Product;
+    @Output() productSelected: EventEmitter<any> = new EventEmitter;
+
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    // ngOnChanges(changes: SimpleChange) {
+    //     console.log('2. ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    ngOnInit() {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. ngDocheck');
+    }
+
+    ngOnDestroy() {
+        console.log('5. ngOnDestroy');
+    }
+
+    selectedProduct() {
+        this.productSelected.emit(this.product.id);
+    }
+}
