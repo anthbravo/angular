6c193de5e562f9f11c9fa74ec9994b25ef50843a
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2019-10-18 12:57:24 -0500

    Se finaliza la etapa de diseño inicial del componente producto y se corrige con la herramienta lint errores pasados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,21 @@ export class AppComponent {
   products: Array<Product> = [
     {
       id: '1',
-      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      image: 'http://lorempixel.com/400/500/',
       title: 'Camisa Casual Azul',
       price: 1000,
       description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
     },
     {
       id: '2',
-      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      image: 'http://lorempixel.com/400/501/',
       title: 'Camisa Casual Azul',
       price: 1000,
       description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
     },
     {
       id: '3',
-      image: 'http://www.solarte.es/image/cache/data/category_12/casual-friday-slim-fit-camisa-informal-hombre-con-botones-camisas-ropa-hombre-ls8faq-3350-500x500.jpg',
+      image: 'http://lorempixel.com/400/502/',
       title: 'Camisa Casual Azul',
       price: 1000,
       description: 'Material 100% algodon. Lavar a maquina a 40°C, no utilizar secadora.'
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -15,12 +15,13 @@ import { Product } from '../product.model';
 @Component(
     {
         selector: 'app-product',
-        templateUrl: './product.component.html'
+        templateUrl: './product.component.html',
+        styleUrls: ['./product.component.css']
     }
 )
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
     @Input() product: Product;
-    @Output() productSelected: EventEmitter<any> = new EventEmitter;
+    @Output() productSelected: EventEmitter<any> = new EventEmitter();
 
     constructor() {
         console.log('1. constructor');
