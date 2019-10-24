db81db276b01e2b7e1ce486f0879919c3f495594
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2019-10-24 16:21:12 -0500

    Se instala el sistema de grillas flexboxgrid

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -15,7 +16,8 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
   ],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MaterialModule
   ]
 })
 export class ProductModule { }
