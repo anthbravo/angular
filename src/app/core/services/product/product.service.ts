9b6dde868b0a091f183c7e862d3ce0498121c81a
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2019-10-25 20:13:29 -0500

    Se implementa CRUD con el API de pruebas

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Product } from '../../model/product.model';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -14,10 +15,22 @@ export class ProductService {
   ) { }
 
   getAllProducts() {
-    return this.httpClient.get<Product[]>('http://platzi-store.herokuapp.com/products');
+    return this.httpClient.get<Product[]>(`${environment.url_api}/products/`);
   }
 
   getProduct(id: string) {
-    return this.httpClient.get<Product>(`http://platzi-store.herokuapp.com/products/${id}`);
+    return this.httpClient.get<Product>(`${environment.url_api}/products/${id}`);
+  }
+
+  createProduct(product: Product) {
+    return this.httpClient.post<Product>(`${environment.url_api}/products`, product);
+  }
+
+  updateProduct(product: Partial<Product>) {
+    return this.httpClient.put<Product>(`${environment.url_api}/products/${product.id}`, product);
+  }
+
+  deleteProduct(id: string) {
+    return this.httpClient.delete(`${environment.url_api}/products/${id}`);
   }
 }
--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -35,4 +35,36 @@ export class ProductDetailComponent implements OnInit {
     );
   }
 
+  createProduct() {
+    const newProduct: Product = {
+      id: '10',
+      title: 'Nuevo Producto',
+      image: 'http://lorempixel.com/400/500/',
+      price: 300,
+      description: 'Es un nuevo producto'
+    };
+
+    this.productService.createProduct(newProduct).subscribe(
+      product => { this.product = product; }
+    );
+  }
+
+  updateProduct() {
+    const newProduct: Partial<Product> = {
+      id: '15',
+      title: 'Nuevo Producto 2',
+      image: 'http://lorempixel.com/400/500/',
+      price: 300
+    };
+
+    this.productService.updateProduct(newProduct).subscribe(
+      product => this.product = product
+    );
+  }
+
+  deleteProduct(id: string) {
+    this.productService.deleteProduct(id).subscribe(
+      respuesta => console.log(respuesta)
+    );
+  }
 }
